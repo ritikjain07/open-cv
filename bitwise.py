a7b3de7f6f65e714371db8e007a1c35212e2b098
ritikjain07 <ritikjain4560@gmail.com>
2025-01-25 16:55:53 +0530

    added bitwise operation

---
+++ bitwise.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((400,400), dtype='uint8')
+rectangle = cv.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1)
+circle = cv.circle(blank.copy(), (200, 200), 200, 255, -1)
+
+cv.imshow('Rectangle', rectangle)
+
+cv.imshow('Circle', circle)
+
+
+# //bitwise and
+
+bitwise_and = cv.bitwise_and(rectangle, circle)
+# cv.imshow('Bitwise AND', bitwise_and)
+
+# bitwise or
+
+bitwise_or = cv.bitwise_or(rectangle, circle)
+# cv.imshow('Bitwise OR', bitwise_or)
+
+# bitwise xor
+
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+# cv.imshow('Bitwise XOR', bitwise_xor)
+
+# bitwise not
+
+bitwise_not_rectangle = cv.bitwise_not(rectangle)
+cv.imshow('Bitwise NOT Rectangle', bitwise_not_rectangle)
+
+bitwise_not_circle = cv.bitwise_not(circle)
+cv.imshow('Bitwise NOT Circle', bitwise_not_circle)
+
+cv.waitKey(0)
